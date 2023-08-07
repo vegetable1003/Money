735a6bb86d3d449fa18f089289e72cc4afafec0f
vegetable1003 <139301208+vegetable1003@users.noreply.github.com>
2023-08-07 10:55:33 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app= Flask(__name__)
 line_bot_api= LineBotApi("6g1a7vGDrHnSn345ATYX+4jDgimHdpoAlN/e2yY2o717kFNWiujz7EMEstiEo+Lng6G0IGpNC2HPAXmU95roOXyGCsDH3NAxSyksHerrVMfyAkQJw+wnCsjaC8JiM2U9uwJqzbUBlgXUGpbSy3vMdAdB04t89/1O/w1cDnyilFU=")
 handler=WebhookHandler("4d45ff441a8509be7dea6fca607ac7cb")
 
-@app.route("/callback",method=["POST"])
+@app.route("/callback",methods=["POST"])
 def callback():
     signature= request.headers['X-Line-Signature']
 
