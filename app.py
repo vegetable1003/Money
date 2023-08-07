0bc61448a39964f4e6c30dec57983d96e9e27528
vegetable1003 <139301208+vegetable1003@users.noreply.github.com>
2023-08-07 14:59:45 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -28,6 +28,30 @@ def handle_message(event):
     if message_text =='@使用說明':
         about_us_event(event)
         Usage(event)
-
+    
+    if event.message.text=='@使用說明':
+        buttons_template = TemplateSendMessage(
+            alt_text="小幫手 template",
+            template=ButtonsTemplate(
+                title="選擇服務",
+                text="請選擇",
+                thumbnail_image_url="https://i.imgur.com/Z16KWAB.gif",
+                actions=[
+                    MessageTemplateAction(
+                        label="油價查詢",
+                        text="油價查詢"
+                    ),
+                     MessageTemplateAction(
+                        label="匯率查詢",
+                        text="匯率查詢"
+                    ),
+                     MessageTemplateAction(
+                        label="股價查詢",
+                        text="股價查詢"
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token,buttons_template)
 if __name__ =="__main__":
     app.run()
