c191ba1f64eaa0692c4a23a9cd620ea28989d289
vegetable1003 <139301208+vegetable1003@users.noreply.github.com>
2023-08-09 16:52:53 +0800

    Add files via upload

--- events/EXRate.py
+++ events/EXRate.py
@@ -1,3 +1,4 @@
+
 import requests
 
 def getCurrencyName(currency):
@@ -25,21 +26,24 @@ def getCurrencyName(currency):
     except: return "無可支援的外幣"
     return currency_name
 
-def getExchangeRate(msg):   #不同貨幣直接換算(非台幣)
+def getExchangeRate(msg): #不同貨幣直接換算(非台幣)
     """
-    sample
-    code = '換匯USD/TWD/100;
+    Sample
+    code = '換匯USD/TWD/100'
     code = '換匯USD/JPY/100'
     """
-    currency_list= msg[2:].split("/")
-    currency=currency_list[0]  #輸入想查詢的匯率
-    currency1= currency_list[1] #輸入想兌換的匯率
-    money_value=currency_list[2] #輸入金額數值
-    url_coinbase='https://api.coinbase.com/v2/exchange-rates?currency=' + currency
-    res=requests.get(url_coinbase)
-    jData=res.json()
-    pd_currency=jData['data']['rates']
-    content=f"目前的兌換率為 : {pd_currency[currency1]}{currency1} \n查詢的金額為 :"
+    currency_list = msg[2:].split("/")
+    currency = currency_list[0]#輸入想查詢的匯率
+    currency1 = currency_list[1] #輸入想兌換的匯率
+    money_value = currency_list[2]#輸入金額數值
+    url_coinbase = "https://api.coinbase.com/v2/exchange-rates?currency=" + currency
+
+    res = requests.get(url_coinbase)
+    jData = res.json()
+    pd_currency = jData["data"]["rates"]
+    content = f"目前對匯率為: {float(pd_currency[currency1]):.2f} {currency1} \n查詢金額為: "
     amount = float(pd_currency[currency1])
-    content += str('%.2f' %(amount *float(money_value)))  + " "+currency1
+
+    content += str(f"{amount*float(money_value):.2f}") + " " + currency1
+
     return content
