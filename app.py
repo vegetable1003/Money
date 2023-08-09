bcb01624de3c9ff07da557d2d25387f230bb0442
vegetable1003 <139301208+vegetable1003@users.noreply.github.com>
2023-08-09 10:30:47 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def handle_message(event):
 
     ##股價查詢
 
-    if re.match("想知道股價[0-9]",msg):
+    if re.match("想知道股價",msg):
         stockNumber=msg[2:6]
         btn_msg=stock_reply_other(stockNumber)
         line_bot_api.push_message(uid,btn_msg)
