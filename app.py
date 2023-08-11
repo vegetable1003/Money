5d31a830d1c3795550e03b474db6c3dff809ce74
vegetable1003 <139301208+vegetable1003@users.noreply.github.com>
2023-08-11 15:46:06 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def handle_message(event):
             url = 'https://tw.stock.yahoo.com/q/q?s=' + stock
             list_req = requests.get(url)
             soup = BeautifulSoup(list_req.content, "html.parser")
-            getstock= soup.findAll('b')[1].text
+            getstock= soup.findAll('span')[1].text
             content = stock + "當前股市價格為: " +  getstock
             if condition == '<':
                 content += "\n篩選條件為: < "+ price
