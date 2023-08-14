8660521d2ee0f9aaec7d376518c016c59f03ad63
vegetable1003 <139301208+vegetable1003@users.noreply.github.com>
2023-08-14 13:39:55 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -22,6 +22,21 @@ def cache_users_stock():
         users.append(cel)
     return users
 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.message.text == 'ID?' or event.message.text == 'id?':
+        User_ID = TextMessage(text=event.source.user_id)
+        line_bot_api.reply_message(event.reply_token, User_ID)
+        print ('Reply User ID =>' + event.source.user_id)
+    elif event.message.text == 'GroupID?':
+        Group_ID = TextMessage(text=event.source.group_id)
+        line_bot_api.reply_message(event.reply_token, Group_ID)
+        print ('Reply Group ID =>' + event.source.group_id)
+    else:
+        None
+    
+line_bot_api.push_message("USER ID or GROUP ID",TextSendMessage(text=""))
+
 #監聽所有來自 /callback的post request
 @app.route("/callback", methods=["POST"])
 def callback():
@@ -37,6 +52,8 @@ def callback():
         abort(400)
 
     return "OK"
+
+
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
